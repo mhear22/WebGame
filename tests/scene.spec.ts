15688fdcaeebc0b3ca0ce80c30b4f229cdcf40a4
McKay <mhear22@gmail.com>
2019-10-15 18:49:08 +1000

    Extracted tests from code

--- tests/scene.spec.ts
+++ tests/scene.spec.ts
@@ -1,7 +1,7 @@
-import { SceneBase } from "./SceneBase";
-import { KeyController } from "../Services/KeyController";
-import { CameraController } from "../Services/CameraController";
-import { Cube } from "../Objects/Cube";
+import {SceneBase} from "../src/Scenes/SceneBase"
+import { KeyController } from "../src/Services/KeyController";
+import { CameraController } from "../src/Services/CameraController";
+import {Cube } from "../src/Objects/Cube";
 
 class TestScene extends SceneBase {
 	public Iterate(keyController: KeyController, Step: number): void {
