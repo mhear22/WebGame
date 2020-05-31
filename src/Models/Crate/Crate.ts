7d7862358c2e99ab8e0233da02c3c4a19a901b0a
McKay <mhear22@gmail.com>
2020-05-31 17:37:14 +1000

    Added floating collectables

---
+++ src/Models/Crate/Crate.ts
@@ -0,0 +1,45 @@
+import { FileAsset } from "../../Objects/FileAsset";
+import { KeyController } from "../../Services/KeyController";
+import { CameraController } from "../../Services/CameraController";
+
+export class CrateModel extends FileAsset {
+	constructor(
+		private Camera: CameraController,
+		private x: number = 0,
+		private y: number = 5,
+		private z: number = -10
+	) {
+		super(require("./Crate.obj"))
+	}
+	
+	private isClose = false;
+	public IsCollected = false;
+	
+	OnLoaded() {
+		this.element.position.x = this.x;
+		this.element.position.y = this.y;
+		this.element.position.z = this.z;
+		this.element.rotateY(-45);
+	}
+	
+	Interval(keyController: KeyController, timeSplit: number): void {
+		if(this.element) {
+			this.element.rotateY(timeSplit)
+			this.element.rotateX(timeSplit/2)
+		}
+		
+		var dist = this.element.position.distanceTo(this.Camera.camera.position);
+		var isNowClose = false;
+		if(dist < 5) {
+			isNowClose = true;
+		}
+		
+		if(isNowClose != this.isClose) {
+			if(isNowClose) {
+				this.IsCollected = true;
+				this.element.visible = false;
+			}
+		}
+		this.isClose = isNowClose;
+	}
+}
--- src/Scenes/BankScene.ts
+++ src/Scenes/BankScene.ts
@@ -7,18 +7,29 @@ import { Sun } from "../Objects/Sun";
 import { Skybox } from "../Objects/Skybox";
 import * as three from 'three';
 import { Rain } from "../Objects/Rain";
+import { CrateModel } from "../Models/Crate/Crate";
 
 
 export class BankScene extends SceneBase {
 	public Iterate(keyController: KeyController, Step: number): void {
 		this.ColideIterate(keyController, Step);
 		this.rain.Element.position.copy(this.Camera.camera.position);
-		this.sun.Element.position.x = this.Camera.camera.position.x
-		this.sun.Element.position.z = this.Camera.camera.position.z
+		this.sun.Element.position.x = this.Camera.camera.position.x;
+		this.sun.Element.position.z = this.Camera.camera.position.z;
+		
+		if(!this.isComplete && this.crates.every(x=>x.IsCollected)) {
+			this.isComplete = true;
+			
+			console.log("Complete")
+		}
 	}
+	
+	private isComplete = false;
 	
 	private rain:Rain;
 	private sun:Sun;
+	
+	private crates:CrateModel[] = []
 	
 	constructor(
 		cam:CameraController,
@@ -29,7 +40,18 @@ export class BankScene extends SceneBase {
 		
 		this.sun = new Sun(this.Scene,0,100,0,null,null, 2);
 		
+		
 		this.Add(new BankModel(60, new three.Vector3(0,-10,100)));
+		
+		this.crates = [
+			new CrateModel(this.Camera,100,5,0),
+			new CrateModel(this.Camera,-50,5,0),
+			new CrateModel(this.Camera,40,35,246)
+		]
+		
+		this.crates.map(x=> { this.Add(x)})
+		
+		
 		this.Add(this.sun)
 		this.Add(new Skybox(cam));
 		this.rain = new Rain(
