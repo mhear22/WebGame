ac908947ccc56ce227ab2fd563ce04203cbdd39d
McKay <mhear22@gmail.com>
2020-05-31 15:32:31 +1000

    updated raycasting pos

--- src/Services/PlayerService.ts
+++ src/Services/PlayerService.ts
@@ -61,46 +61,91 @@ export class PlayerService extends ServiceBase {
 		}, 100)
 	}
 
+	private MovementDirection(timeSplit: number) {
+		if (this.Key.KeyMap["shift"]) {
+			this.MovementSpeed++;
+			if (this.MovementSpeed >= 5)
+				this.MovementSpeed = 5;
+		}
+		else {
+			if (this.MovementSpeed > 1)
+				this.MovementSpeed--;
+			else
+				this.MovementSpeed = 1;
+		}
+
+		if (this.Key.KeyMap["w"])
+			return new Vector3(0, 0, -timeSplit * this.MovementSpeed * 10);
+		if (this.Key.KeyMap["s"])
+			return new Vector3(0, 0, timeSplit * this.MovementSpeed * 10);
+		if (this.Key.KeyMap["a"])
+			return new Vector3(-timeSplit * this.MovementSpeed * 10, 0, 0);
+		if (this.Key.KeyMap["d"])
+			return new Vector3(timeSplit * this.MovementSpeed * 10, 0, 0);
+		return new Vector3(0,0,0)
+	}
+	
+	private intersection(camPos: three.Vector3, direction: three.Vector3) {
+		var ray = new three.Raycaster(camPos, direction, 0, 100);
+		var meshes = this.GetScene().CollideMeshes;
+		var rays = ray.intersectObjects(meshes).filter(x => x.distance > 0).sort(x => x.distance);
+		return rays[0];
+	}
+	
 	public Iterate(timeSplit: number) {
 		if(PlayerService.Gravity) {
 			var scene = this.GetScene()
 			var meshes = scene.CollideMeshes;
 			var pos = this.Camera.camera.position.clone()
 			pos.y -= 8;
-			var ray = new three.Raycaster(pos, new three.Vector3(0, -1, 0), 0, 100);
-			var rays = ray.intersectObjects(meshes).filter(x => x.distance > 0).sort(x => x.distance);
-			if (rays[0]) {
-				var floor = rays[0];
-				if (floor.distance > 1) {
-					this.Camera.camera.position.y -= ((rays[0].distance) * 0.99);
+			var ray = this.intersection(pos, new three.Vector3(0,-1,0))
+			
+			if (ray) {
+				if (ray.distance > 1) {
+					this.Camera.camera.position.y -= ((ray.distance) * 0.99);
 				}
 			}
 			else
 				this.Camera.camera.position.y += 0.1;
 		}
-
-		if (PlayerService.WalkingControls) {
-			if (this.Key.KeyMap["shift"]) {
-				this.MovementSpeed++;
-				if (this.MovementSpeed >= 5)
-					this.MovementSpeed = 5;
-			}
-			else {
-				if (this.MovementSpeed > 1) {
-					this.MovementSpeed--;
+		
+		/*
+		if(PlayerService.Gravity) {
+			var pos = this.Camera.camera.position.clone()
+			pos.y -= 8;
+			
+			var ray = this.intersection(pos, new three.Vector3(0,-1,0))
+			
+			if (ray) {
+				//Over the floor
+				var floor = ray;
+				if (floor.distance > 0.5) {
+					this.Camera.camera.position.y -= ray.distance/10;
 				}
 				else
-					this.MovementSpeed = 1;
+					this.Camera.camera.position.y -= ray.distance;
 			}
-
-			if (this.Key.KeyMap["w"])
-				this.Move(new Vector3(0, 0, -timeSplit * this.MovementSpeed * 10));
-			if (this.Key.KeyMap["s"])
-				this.Move(new Vector3(0, 0, timeSplit * this.MovementSpeed * 10));
-			if (this.Key.KeyMap["a"])
-				this.Move(new Vector3(-timeSplit * this.MovementSpeed * 10, 0, 0));
-			if (this.Key.KeyMap["d"])
-				this.Move(new Vector3(timeSplit * this.MovementSpeed * 10, 0, 0));
+			else {
+				pos.y += 1
+				var floorAbove = this.intersection(pos, new three.Vector3(0,-1,0))
+				if(floorAbove) {
+					if(floorAbove.distance < 1) {
+						var actualDist = Math.abs(floorAbove.distance - 1)
+						this.Camera.camera.position.y += actualDist;
+					}
+				}
+				else
+					this.Camera.camera.position.y += 0.1;
+				//Under the floor
+				//console.log("Flying up")
+				//flying up
+			}
+				
+		}
+		*/
+		
+		if(PlayerService.WalkingControls) {
+			this.Move(this.MovementDirection(timeSplit));
 		}
 	}
 	public Move(vector: Vector3) {
